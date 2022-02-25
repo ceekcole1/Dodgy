ed47a5e17556adb144636ce66f265120a9d551dd
choi5907 <99639267+choi5907@users.noreply.github.com>
2022-02-25 17:30:23 +0900

    20220225

--- Dodge/Assets/PlayerController.cs
+++ Dodge/Assets/PlayerController.cs
@@ -7,13 +7,15 @@ public class PlayerController : MonoBehaviour
     // 이동에 사용할 리지드바디 컴포넌트
     public Rigidbody playerRigidbody;
     // 이동 속력
-    public float speed = 8f;
+    public float speed = 3f;
 
     // 내 자신을 담을 변수
     public GameObject my;
 
     private void Start()
     {
+        // 게임 오브젝트에서 Rigidbody 컴포넌트를 찾아
+        // playerRigidbody에 할당
         playerRigidbody = GetComponent<Rigidbody>();
     }
 
@@ -21,10 +23,21 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-        {
+
+       {
+        // 수평축과 수직축의 입력값을 감지해서 저장
         float xInput = Input.GetAxis("Horizontal");
 
         float zInput = Input.GetAxis("Vertical");
+
+        float xspeed = xInput * speed;
+        float zspeed = zInput * speed;
+
+        Vector3 newVelocity = new Vector3(xspeed, 0, zspeed);
+
+        playerRigidbody.velocity = newVelocity;
+
+
         }  
 
         void DirectInput()
@@ -47,7 +60,7 @@ public class PlayerController : MonoBehaviour
             playerRigidbody.AddForce(-speed, 0f, 0f);
         }
        }
-        void Die() 
+        public void Die() 
         {
             my.SetActive(false);
           
